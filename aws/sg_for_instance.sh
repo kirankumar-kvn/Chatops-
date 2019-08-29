2d21c666c75060f0448ffda6c5e467e20c3ba3fc
root <root@ip-10-0-1-250.us-west-2.compute.internal>
2019-08-29 13:22:41 +0000

    chatops_scripts

---
+++ aws/sg_for_instance.sh
@@ -0,0 +1,2 @@
+aws ec2 describe-security-groups --group-ids $(aws ec2 describe-instances --instance-ids $1 --query "Reservations[].Instances[].SecurityGroups[].GroupId[]" --output text) --output table > /home/slack_op/aws.txt
+ansible 127.0.0.1 -m shell -a "curl -F file=@/home/slack_op/aws.txt -F channels=#chatops -F token=xoxb-723276734437-722837300500-a2gZ1gnJDA5kxENOGZ9n8COm https://slack.com/api/files.upload -s >> /dev/null"
---
+++ docker/docker_container.sh
@@ -0,0 +1,3 @@
+docker container ps > /home/slack_op/docker.txt
+ansible 127.0.0.1 -m shell -a "curl -F file=@/home/slack_op/docker.txt -F channels=#chatops -F token=xoxb-723276734437-722837300500-a2gZ1gnJDA5kxENOGZ9n8COm https://slack.com/api/files.upload -s >> /dev/null"
+
